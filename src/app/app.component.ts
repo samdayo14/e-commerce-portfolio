7d18c434a36fc6083d1c836ada06c2d1d49383ac
Samuel Adeniran <samueldayo618@gmail.com>
2026-01-20 12:10:50 +0100

    feat: implement footer component with layout and styling, update app component for conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  RouterOutlet,
+  RouterLink,
+  Router,
+  NavigationEnd,
+} from '@angular/router';
+import { FooterComponent } from './shared/components/layout/footer/footer.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecommerce-portfolio';
+export class AppComponent implements OnInit {
+  showLayout = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const hiddenRoutes = ['/login', '/register', '/forgot-password'];
+
+        const isAuthPage = hiddenRoutes.some((route) =>
+          event.urlAfterRedirects.includes(route)
+        );
+
+        this.showLayout = !isAuthPage;
+      });
+  }
 }
